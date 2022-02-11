5ee688874ac34d3d58607086d4b2b414224bb0ba
aliaksei.zhemblouski.epam <Aliaksei_Zhemblouski@epam.com>
2022-02-11 10:51:06 +0300

    Add REST-HTTP examples and flash app

---
+++ REST-HTTP/upload.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['TESTING'] = True
+
+@app.route('/lol', methods = ['GET'])
+def lol():
+    return 'success'
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      return str(request.values)
+      #f = request.files['data']
+      #f.save(f.filename)
+      #return 'file uploaded successfully'
+   else:
+      return 'Only Post requests are allowed'
+
+if __name__ == '__main__':
+   app.testing = True
+   app.run(debug = True)
