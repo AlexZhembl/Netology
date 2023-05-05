2bb67de1490619c996214d30eaf73886ac01ac02
aliaksei.zhemblouski.epam <Aliaksei_Zhemblouski@epam.com>
2023-05-05 16:01:15 +0200

    Update trening 4 task

--- Trening/2.2/InitialCode/task4.swift
+++ Trening/2.2/InitialCode/task4.swift
@@ -18,14 +18,27 @@ enum Month {
     // Дополните switch, замение "..." на необходимый код
     var description: String {
         switch self {
-        case ...: return "Январь"
-        case .february: return ...
-        ...
+        case .january: return "Январь"
+        case .february: return "Февраль"
+        case .march: return "Март"
+        case .april: return "Апрель"
+        case .may: return "Май"
+        case .june: return "Июнь"
+        case .july: return "Июль"
+        case .august: return "Август"
+        case .september: return "Сентябрь"
+        case .october: return "Октябрь"
+        case .november: return "Ноябрь"
+        case .december: return "Декабрь"
         }
     }
 
     // Опишите новый switch для количества выходных дней
     var numberOfDays: Int {
-        ...
+        switch self {
+        case .january: return 16
+        case .february, .march, .may, .june, .november: return 9
+        default: return 8
+        }
     }
 }
