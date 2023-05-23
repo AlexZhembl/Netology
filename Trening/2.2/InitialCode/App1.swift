42c8dfc7952568019cfdabb87278d767d75da134
aliaksei.zhemblouski.epam <Aliaksei_Zhemblouski@epam.com>
2023-05-23 16:14:19 +0200

    Trening: add app3 && app2

---
+++ Trening/2.2/InitialCode/App1.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+@main
+public struct App {
+
+    public static func main() {
+        let solution = Solution()
+        let res = solution.howManyDays(in: "Март")
+        print(res)
+    }
+}
