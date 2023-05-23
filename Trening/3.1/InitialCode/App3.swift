b8ba62dabb4b8887838e68e5927c1fd974aac294
aliaksei.zhemblouski.epam <Aliaksei_Zhemblouski@epam.com>
2023-05-23 16:31:13 +0200

    Trening: 3.1  add app3

--- Trening/3.1/InitialCode/App3.swift
+++ Trening/3.1/InitialCode/App3.swift
@@ -5,8 +5,7 @@ public struct App {
 
     public static func main() {
         let solution = Solution()
-        let girls = ["Inna, Vlada, Karolina"]
-        let result = solution.sortArray(of: ["Ivan, Petr, Samuil"], girls: girls)
+        let result = solution.filterArray(of: [1, 5 , -10, 9, 4, 2, 0 , -4])
         print("Результат решения: \(result)")
     }
 }
