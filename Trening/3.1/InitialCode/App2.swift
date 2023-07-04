b2a5e783846f25d9daba104deb93042a5d877f82
AlexZhembl <alex.zhemblow@gmail.com>
2023-07-04 14:46:05 +0200

    Update App2.swift

--- Trening/3.1/InitialCode/App2.swift
+++ Trening/3.1/InitialCode/App2.swift
@@ -5,8 +5,8 @@ public struct App {
 
     public static func main() {
         let solution = Solution()
-        let girls = ["Inna, Vlada, Karolina"]
-        let result = solution.sortArray(of: ["Ivan, Petr, Samuil"], girls: girls)
+        let girls = ["Inna", "Vlada", "Karolina"]
+        let result = solution.sortArray(of: ["Ivan", "Petr", "Samuil"], girls: girls)
         print("Результат решения: \(result)")
     }
 }
